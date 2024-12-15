09165d3f28ce0fece00851011946d41632f8def8
Franqueli123 <franco.jose.ib@gmail.com>
2024-12-15 16:30:41 -0300

    se borran tres archivos de prueba playwrith

--- tests/nuevoTest.spec.js
+++
@@ -1,9 +0,0 @@
-const { test, expect } = require('@playwright/test');
-const { HomePage } = require('../POM/HomePage');
-
-
-test('has title', async ({ page }) => {
-
-  const homePage = new HomePage(page)
-  
-})
