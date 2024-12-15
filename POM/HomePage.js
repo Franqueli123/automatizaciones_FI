3d659acc3a41cfab8ae835cb21f510b1c8868680
Franqueli123 <franco.jose.ib@gmail.com>
2024-12-15 16:11:57 -0300

    segundo commit

---
+++ POM/HomePage.js
@@ -0,0 +1,53 @@
+const { expect } = require('@playwright/test');
+
+exports.HomePage = class HomePage {
+
+  /**
+   * @param {import('@playwright/test').Page} page
+   */
+  constructor(page) {
+    this.page = page;
+    this.locatorBotonSign = page.locator("xpath=(//span[@class='hidden-sm-down'])[1]")
+    this.localStorage = page.locator("xpath=//input[@id='sign-username']")
+    this.localStorage2 = page.locator("xpath=//input[@id='sign-password']")
+    this.locatorBotonSign2 = page.locator("xpath=//button[@onclick='register()']")
+  }
+
+  async goto() {
+    await this.page.goto('https://teststore.automationtesting.co.uk/index.php');
+  }
+
+  async getStarted() {
+    await this.getStartedLink.first().click();
+    await expect(this.gettingStartedHeader).toBeVisible();
+  }
+
+  async pageObjectModel() {
+    await this.getStarted();
+    await this.pomLink.click();
+  }
+
+  async clickEnIngresar(){
+    await this.locatorBotonSign.click()
+  }
+
+  async rellenarNomnbre(nombre){
+    await this.localStorage.fill(nombre)
+  }
+
+  async rellenarPass(pass){
+    await this.localStorage2.fill(pass)
+  }
+
+  async clickBotonSignIn(){
+    await this.locatorBotonSign2.click()
+  }
+
+  async rellenarFull(name,pass){
+    await this.clickEnIngresar()
+    await this.rellenarNomnbre(name)
+    await this.rellenarPass(pass)
+    await this.clickBotonSignIn()
+  }
+
+};
